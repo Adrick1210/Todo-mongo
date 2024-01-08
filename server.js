dced339b39c34779c31fa0a51cb8746e6ac65dff
Christian Alexander <christian.alexanderato@gmail.com>
2024-01-07 19:09:38 -0800

    index route working and showing test todo with seed data

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const express = require("express");
 const morgan = require("morgan");
 const methodOverride = require("method-override");
 const { PORT = 3030 } = process.env;
-const Todo = require("./models/todo.js");
+const Todo = require("./models/todos.js");
+const seedData = require("./models/seed.js");
 
 // APP OBJECT
 const app = express();
@@ -13,7 +14,7 @@ const app = express();
 // MIDDLE WARE
 app.use(morgan("dev"));
 app.use(methodOverride("method-override"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static("public"));
 
 // ROUTES
@@ -23,9 +24,27 @@ app.get("/", (req, res) => {
   res.send("Hello Todo");
 });
 
+// Seed
+app.get("/todos/seed", async (req, res) => {
+  try {
+    await Todo.deleteMany({});
+    const todos = await Todo.create(seedData);
+    res.json(todos);
+  } catch (error) {
+    console.log(error.message);
+    res.send("Theres a issue with the seeds");
+  }
+});
+
 // Index
-app.get("/todos", (req,res) => {
-    res.render("index.ejs");
+app.get("/todos", async (req, res) => {
+  try {
+    let todos = await Todo.find({});
+    res.render("index.ejs", { todos });
+  } catch (error) {
+    console.log("-----", error.message, "-----");
+    res.status(400).send("error, read logs for error details");
+  }
 });
 
 // New
